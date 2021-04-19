49a9bc4ad73be87d4f90bb45ce85deb98074026d
mikaells <mikael.lenz.strube@gmail.com>
2021-04-19 19:19:47 +0200

    Update run16sSummary.R
    Added code to handle --ANI option in main

--- run16sSummary.R
+++ run16sSummary.R
@@ -23,60 +23,69 @@ NZ=paste(splitname[3:4], collapse='_')
 gen=splitname[5]
 spec=paste(splitname[6:length(splitname)-1],collapse=' ')
 
-mismatch=read.table(mismatchPath, skip=1)[,-1]
-n=NROW(mismatch)
-
-if(n>1) {
-
-upperMismatch=mismatch[upper.tri(mismatch)]
-
-mu=round(mean(upperMismatch),2)
-sd=round(sd(  upperMismatch),2)
-
-mx=max(upperMismatch)
-mi=min(upperMismatch)
 
 
-#address of fasta file
-fasta=read.fasta(alignmentPath)
-
-#turning into a matrix
-DNA_mat=do.call(rbind,fasta)
-
-#finding dimensions of data
-max_len=NCOL(DNA_mat)
-n_seqs=NROW(DNA_mat)
-
-#making empty vector for diversity across string
-divs=rep(-1, max_len) 
-
-#looping across all nucleotide positions
-for( i in 1:max_len) {
-  #turn each position into a table with of counts for each nucleotide, while including 0s.
-  per_pos=factor(DNA_mat[,i], levels=c("A","G", "C", "T", "-"))
-  probs=table(per_pos)/n_seqs
-  divs[i]=-sum(probs*ifelse(log(probs)==-Inf, 0, log(probs))) #shannon index
-}
-
-totalDiv=sum(divs)
-
-cat(paste(GCF,gen,spec, n, mu,sd,mi,mx, totalDiv,'\n',sep='\t')) 
-
-#calculation rolling mean of diversity
-roll_means_30=rollmean(divs, k = 30)
-
-tree=read.tree(treeFile)
-
-
-pdf(file = pngOut,width = 7.4,height = 5)
-par(mfrow=c(1,2))
-
-plot(divs, pch=16, cex=0.1, type="p", main="Diversity", xlab="Nucleotide position", ylab="Diversity")
-lines(roll_means_30, col=3)
-legend("topright", legend = "Rolling mean", col=3, lty=1)
-plot(tree)
-dummy=suppressMessages(dev.off())
-
+if(file.exists(mismatchPath)) {
+  
+  
+  mismatch=read.table(mismatchPath, skip=1)[,-1]
+  n=NROW(mismatch)
+  
+  if(n>1) {
+    
+    upperMismatch=mismatch[upper.tri(mismatch)]
+    
+    mu=round(mean(upperMismatch),2)
+    sd=round(sd(  upperMismatch),2)
+    
+    mx=max(upperMismatch)
+    mi=min(upperMismatch)
+    
+    
+    #address of fasta file
+    fasta=read.fasta(alignmentPath)
+    
+    #turning into a matrix
+    DNA_mat=do.call(rbind,fasta)
+    
+    #finding dimensions of data
+    max_len=NCOL(DNA_mat)
+    n_seqs=NROW(DNA_mat)
+    
+    #making empty vector for diversity across string
+    divs=rep(-1, max_len) 
+    
+    #looping across all nucleotide positions
+    for( i in 1:max_len) {
+      #turn each position into a table with of counts for each nucleotide, while including 0s.
+      per_pos=factor(DNA_mat[,i], levels=c("A","G", "C", "T", "-"))
+      probs=table(per_pos)/n_seqs
+      divs[i]=-sum(probs*ifelse(log(probs)==-Inf, 0, log(probs))) #shannon index
+    }
+    
+    totalDiv=sum(divs)
+    
+    cat(paste(GCF,gen,spec, n, mu,sd,mi,mx, totalDiv,'\n',sep='\t')) 
+    
+    #calculation rolling mean of diversity
+    roll_means_30=rollmean(divs, k = 30)
+    
+    tree=read.tree(treeFile)
+    
+    
+    pdf(file = pngOut,width = 7.4,height = 5)
+    par(mfrow=c(1,2))
+    
+    plot(divs, pch=16, cex=0.1, type="p", main="Diversity", xlab="Nucleotide position", ylab="Diversity")
+    lines(roll_means_30, col=3)
+    legend("topright", legend = "Rolling mean", col=3, lty=1)
+    plot(tree)
+    dummy=suppressMessages(dev.off())
+    
+  } else {
+    cat(paste(GCF,gen,spec, n, 0,0,0,0, 0,'\n',sep='\t')) 
+  } 
+  
 } else {
-	cat(paste(GCF,gen,spec, n, 0,0,0,0, 0,'\n',sep='\t')) 
+  cat(paste(GCF,gen,spec, 0, 0,0,0,0, 0,'\n',sep='\t')) 
 }

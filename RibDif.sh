adfcfaf8f321eb18b532a767427be620cf067a40
mikaells <mikael.lenz.strube@gmail.com>
2021-05-18 20:32:44 +0200

    Update RibDif.sh

--- RibDif.sh
+++ RibDif.sh
@@ -100,7 +100,7 @@ then
 fi
 
 #save command line
-cat "RibDif.sh --genus $genus --clobber $clobber --ANI $ANI --frag $frag --id $id --threads Ncpu" > $genus/run_cmd
+echo "RibDif.sh --genus $genus --clobber $clobber --ANI $ANI --frag $frag --id $id --threads Ncpu" > $genus/run_cmd
 
 
 #Use ncbi-genome-download for downloading genus/species

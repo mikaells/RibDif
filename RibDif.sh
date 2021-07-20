31bab56dce79296583b1488dd1afc78a9917c89a
mikaells <mikael.lenz.strube@gmail.com>
2021-07-20 10:13:59 +0200

    Update RibDif.sh

--- RibDif.sh
+++ RibDif.sh
@@ -134,7 +134,7 @@ then
 fi
 
 #save command line
-echo "RibDif.sh --genus $genus --clobber $clobber --ANI $ANI --frag $frag --id $id --threads $Ncpu" > $genus/run_cmd
+echo "RibDif.sh --genus $genus --primers $primers --clobber $clobber --ANI $ANI --frag $frag --id $id --threads $Ncpu" > $genus/run_cmd
 
 #gunzip all in parallel
 echo -e "Gunzipping all files.\n\n"
